9cf8abee1f38ddd5aeb965571b072aefa7ba34d0
eddwatts <edd@eddtech.co.uk>
2025-11-06 13:30:32 +0000

    Update BambuLed.ino

--- BambuLed/BambuLed.ino
+++ BambuLed/BambuLed.ino
@@ -13,6 +13,7 @@
 #include <ArduinoOTA.h>
 #include <deque>
 #include <time.h>
+#include <WebSocketsServer.h>
 
 // Include all our module headers
 #include "config.h"
@@ -28,6 +29,7 @@ WebServer server(80);
 WiFiManager wm;
 WiFiClientSecure espClient;
 PubSubClient client(espClient);
+WebSocketsServer webSocket = WebSocketsServer(81);
 
 // Global state variables
 String current_light_mode = "UNKNOWN";
@@ -46,11 +48,8 @@ int current_stage = -1;
 String current_wifi_signal = "N/A";
 unsigned long finishTime = 0;
 const unsigned long FINISH_LIGHT_TIMEOUT = 120000;
-
-// --- FIX ---
-// Added the definition for the MQTT topic string
 String mqtt_topic_status;
-// --- END FIX ---
+unsigned long lastAnimationUpdate = 0;
 
 // Non-blocking reconnect timer
 unsigned long lastReconnectAttempt = 0;
@@ -59,6 +58,41 @@ const unsigned long RECONNECT_INTERVAL = 5000;
 // File upload state
 File restoreFile;
 bool restoreSuccess = false;
+
+void onWebSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length) {
+  switch (type) {
+    case WStype_DISCONNECTED:
+      Serial.printf("[%u] WebSocket Disconnected!\n", num);
+      break;
+    case WStype_CONNECTED: {
+      IPAddress ip = webSocket.remoteIP(num);
+      Serial.printf("[%u] WebSocket Connected from %d.%d.%d.%d\n", num, ip[0], ip[1], ip[2], ip[3]);
+      
+      // When a new client connects, immediately send them the *current* status!
+      // This requires `createStatusJson` to be defined in `web_handlers.h`
+      DynamicJsonDocument doc(1024);
+      createStatusJson(doc); 
+      String json_output;
+      serializeJson(doc, json_output);
+      webSocket.sendTXT(num, json_output);
+      break;
+    }
+    case WStype_TEXT:
+      Serial.printf("[%u] got text: %s\n", num, payload);
+      // Example: You could add logic here to control the light *from* the websocket
+      if (strcmp((char*)payload, "LIGHT_ON") == 0) {
+        Serial.println("WebSocket received LIGHT_ON command");
+        handleLightOn(); // Reuse existing handler
+      } else if (strcmp((char*)payload, "LIGHT_OFF") == 0) {
+        Serial.println("WebSocket received LIGHT_OFF command");
+        handleLightOff();
+      } else if (strcmp((char*)payload, "LIGHT_AUTO") == 0) {
+        Serial.println("WebSocket received LIGHT_AUTO command");
+        handleLightAuto();
+      }
+      break;
+  }
+}
 
 void setup() {
   Serial.begin(115200);
@@ -129,6 +163,26 @@ void setup() {
   // Setup web server
   setupWebServer();
 
+  
+  server.on("/reboot", [](){
+    server.send(200, "text/html", "<!DOCTYPE html><html><head><title>Rebooting...</title><meta http-equiv='refresh' content='5;url=/'></head><body style='font-family:sans-serif; background:#1a1a1b; color:#e0e0e0;'><h2>Rebooting...</h2><p>You will be redirected in 5 seconds.</p></body></html>");
+    delay(1000);
+    ESP.restart();
+  });
+  
+  server.on("/factory_reset", [](){
+    server.send(200, "text/html", "<!DOCTYPE html><html><head><title>Resetting...</title><meta http-equiv='refresh' content='10;url=/'></head><body style='font-family:sans-serif; background:#1a1a1b; color:#e0e0e0;'><h2>Factory Reset...</h2><p>Wiping config and rebooting. The device will restart in AP mode. You will be redirected in 10 seconds.</p></body></html>");
+    performFactoryReset();
+    delay(1000);
+    wm.resetSettings(); // Also reset WiFi
+    delay(1000);
+    ESP.restart();
+  });
+
+  
+  webSocket.begin();
+  webSocket.onEvent(onWebSocketEvent);
+
   Serial.println("--- Setup Complete ---");
 }
 
@@ -138,6 +192,9 @@ void loop() {
 
   // Handle web client requests
   server.handleClient();
+  
+  webSocket.loop(); 
+  
   // Handle MQTT connection
   handleMQTTConnection();
 
