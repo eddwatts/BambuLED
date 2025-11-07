8fc05e69edfc8fa855b309adbfa81cc077ea89b5
eddwatts <edd@eddtech.co.uk>
2025-11-07 08:24:49 +0000

    Update BambuLed.ino

--- BambuLed/BambuLed.ino
+++ BambuLed/BambuLed.ino
@@ -13,7 +13,7 @@
 #include <ArduinoOTA.h>
 #include <deque>
 #include <time.h>
-#include <WebSocketsServer.h>
+#include <WebSocketsServer.h> // Added for WebSockets
 
 // Include all our module headers
 #include "config.h"
@@ -29,7 +29,7 @@ WebServer server(80);
 WiFiManager wm;
 WiFiClientSecure espClient;
 PubSubClient client(espClient);
-WebSocketsServer webSocket = WebSocketsServer(81);
+WebSocketsServer webSocket = WebSocketsServer(81); // Added for WebSockets
 
 // Global state variables
 String current_light_mode = "UNKNOWN";
@@ -49,6 +49,8 @@ String current_wifi_signal = "N/A";
 unsigned long finishTime = 0;
 const unsigned long FINISH_LIGHT_TIMEOUT = 120000;
 String mqtt_topic_status;
+
+// Added for LED Animations
 unsigned long lastAnimationUpdate = 0;
 
 // Non-blocking reconnect timer
@@ -59,6 +61,7 @@ const unsigned long RECONNECT_INTERVAL = 5000;
 File restoreFile;
 bool restoreSuccess = false;
 
+// --- WebSocket Event Handler ---
 void onWebSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length) {
   switch (type) {
     case WStype_DISCONNECTED:
@@ -68,8 +71,7 @@ void onWebSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t leng
       IPAddress ip = webSocket.remoteIP(num);
       Serial.printf("[%u] WebSocket Connected from %d.%d.%d.%d\n", num, ip[0], ip[1], ip[2], ip[3]);
       
-      // When a new client connects, immediately send them the *current* status!
-      // This requires `createStatusJson` to be defined in `web_handlers.h`
+      // When a new client connects, immediately send them the *current* status
       DynamicJsonDocument doc(1024);
       createStatusJson(doc); 
       String json_output;
@@ -79,16 +81,19 @@ void onWebSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t leng
     }
     case WStype_TEXT:
       Serial.printf("[%u] got text: %s\n", num, payload);
-      // Example: You could add logic here to control the light *from* the websocket
+      // Handle light control commands from WebSocket
       if (strcmp((char*)payload, "LIGHT_ON") == 0) {
         Serial.println("WebSocket received LIGHT_ON command");
         handleLightOn(); // Reuse existing handler
+        broadcastWebSocketStatus(); // Push update back
       } else if (strcmp((char*)payload, "LIGHT_OFF") == 0) {
         Serial.println("WebSocket received LIGHT_OFF command");
         handleLightOff();
+        broadcastWebSocketStatus(); // Push update back
       } else if (strcmp((char*)payload, "LIGHT_AUTO") == 0) {
         Serial.println("WebSocket received LIGHT_AUTO command");
         handleLightAuto();
+        broadcastWebSocketStatus(); // Push update back
       }
       break;
   }
@@ -108,8 +113,11 @@ void setup() {
     ESP.restart();
   }
 
-  // Add initial boot message to log
-  mqtt_history.push_back("[--:--:--] System Booted. Initializing...");
+  // --- FIX for Highlighted Log ---
+  // Add initial boot message to log using the new struct
+  mqtt_history.push_back({getTimestamp() + " System Booted. Initializing...", true});
+  // --- END FIX ---
+  
   // Handle factory reset
   if (forceReset) {
     performFactoryReset();
@@ -163,9 +171,9 @@ void setup() {
   // Setup web server
   setupWebServer();
 
-  
+  // --- Added for Reboot/Reset buttons ---
   server.on("/reboot", [](){
-    server.send(200, "text/html", "<!DOCTYPE html><html><head><title>Rebooting...</title><meta http-equiv='refresh' content='5;url=/'></head><body style='font-family:sans-serif; background:#1a1a1b; color:#e0e0e0;'><h2>Rebooting...</h2><p>You will be redirected in 5 seconds.</p></body></html>");
+    server.send(200, "text/html", "<!DOCTYPE html><html><head><title>Rebooting...</title><meta http-EVequiv='refresh' content='5;url=/'></head><body style='font-family:sans-serif; background:#1a1a1b; color:#e0e0e0;'><h2>Rebooting...</h2><p>You will be redirected in 5 seconds.</p></body></html>");
     delay(1000);
     ESP.restart();
   });
@@ -179,7 +187,7 @@ void setup() {
     ESP.restart();
   });
 
-  
+  // --- Added for WebSockets ---
   webSocket.begin();
   webSocket.onEvent(onWebSocketEvent);
 
@@ -193,6 +201,7 @@ void loop() {
   // Handle web client requests
   server.handleClient();
   
+  // --- Added for WebSockets ---
   webSocket.loop(); 
   
   // Handle MQTT connection
